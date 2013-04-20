77c1870bc94085a26d9434a92f62e8a11c307053
poorevil <poorevil@gmail.com>
2013-04-21 01:00:36 +0800

    Initial Commit

---
+++ MMPictorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MMPictorial
+//
+//  Created by han chao on 13-4-21.
+//  Copyright (c) 2013年 taoxiaoxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
